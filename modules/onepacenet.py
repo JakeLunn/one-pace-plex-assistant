c91765dbc680aa7b5fd5693f84a3073f7aca827a
Jake Lunn <jakelunn@protonmail.com>
2023-09-10 23:51:25 -0500

    added auto-fetching build id from onepact (fix)

--- modules/onepacenet.py
+++ modules/onepacenet.py
@@ -1,15 +1,26 @@
 """Module for calling the onepace.net API to retrieve episode information"""
+import re
 import requests
+
+BUILD_ID_REGEX = re.compile(r"\"buildId\".+?\"(?P<BuildId>.+?)\"")
 
 
 class TranslationNotFoundException(Exception):
     """Exception for when a translation is not found"""
 
 
+def get_build() -> str:
+    """Get the Build Id from onepace.net"""
+    response = requests.get("https://onepace.net", timeout=10)
+    response.raise_for_status()
+    return BUILD_ID_REGEX.search(response.text).group("BuildId")
+
+
 def get_arcs() -> list:
     """Get the One Pace episodes from the API"""
+    build_id = get_build()
     response = requests.get(
-        "https://onepace.net/_next/data/3ivGuB-QWFxGji410gfuK/en/watch.json", timeout=10
+        f"https://onepace.net/_next/data/{build_id}/en/watch.json", timeout=10
     )
     response.raise_for_status()
     arcs = response.json()["pageProps"]["arcs"]
@@ -23,6 +34,7 @@ def get_image(url: str) -> bytes:
     )
     response.raise_for_status()
     return response.content
+
 
 def extract_from_translations(media, language_code: str, key: str) -> str:
     """Extract the title from the One Pace episode"""
@@ -38,9 +50,11 @@ def extract_from_translations(media, language_code: str, key: str) -> str:
         raise TranslationNotFoundException(f"No {language_code} translation found")
     return translation[key]
 
+
 def extract_title(media) -> str:
     """Extract the title from the One Pace episode"""
     return extract_from_translations(media, "en", "title")
+
 
 def extract_description(media) -> str:
     """Extract the description from the One Pace episode"""
